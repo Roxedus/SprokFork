11a633daa0711ca2275d6830dc568cd827dfebda
Roxedus <me@roxedus.dev>
2019-07-03 23:18:20 +0200

    Fornorsking, preposisjoner og annen info

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from typing import Optional
 from bot import BotSetup
+import aiohttp
 import dataIO
 
 
@@ -25,7 +26,7 @@ class Sprakradet(commands.Cog):
     @commands.command()
     async def list(self, ctx, big: Optional[bool] = False):
         """
-        Lists whitelist of this servertype
+        Lister hvilke låneord som er untatt
         """
         if ctx.guild.large:
             big = True
@@ -43,7 +44,7 @@ class Sprakradet(commands.Cog):
     @commands.command()
     async def wordlist(self, ctx, word, big: Optional[bool] = False):
         """
-        Adds or removes word from blacklist
+        Legger til eller fjerner ord fra untakslisten
         """
         if ctx.guild.large:
             big = True
@@ -63,7 +64,7 @@ class Sprakradet(commands.Cog):
     @commands.command()
     async def channellist(self, ctx, channel: discord.TextChannel = None):
         """
-        Adds or removes channel from blacklist
+        Legger til eller fjerner kanaler fra untakslisten
         """
         if channel is None:
             channel = ctx.message.channel
@@ -82,10 +83,10 @@ class Sprakradet(commands.Cog):
     @commands.command()
     async def rate(self, ctx, num):
         """
-        Sets the rate on hits for this server. 0-100
+        Setter raten for hvor ofte botten skal reagere. 0-100
         """
         if int(num) > 100:
-            await ctx.send("Rate over 100, skipping")
+            await ctx.send("Rate over 100, hopper over")
             return
         BotSetup.settings["rate"][str(ctx.guild.id)] = str(num)
         self.save_json()
@@ -99,5 +100,49 @@ class Sprakradet(commands.Cog):
                        enable_verbose=False)
 
 
+class Preposisjon(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
+
+    @commands.command()
+    async def prep(self, ctx, ord):
+        """
+        Bruker https://github.com/draperunner/preposisjon for å finne preosisjon.
+        """
+        async with self.session.get(f"https://preposisjon.no/api?place={ord}", timeout=30) as response:
+            assert response.status == 200
+            p = await response.json()
+            await ctx.send(f"Du er {p['preposisjon']} {ord.title()}")
+
+class Info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.repo = "https://github.com/Roxedus/SprokFork"
+
+    @commands.command()
+    async def info(self, ctx):
+        """
+        Lister info om botten, og serverrelaterte instillinger
+        """
+        desc = f"Discord-programvareagent som reagerer på bruken av lånord, og forslår norske alternativer. " \
+               f"Forbedringforslag mottas på [GitHub]({self.repo})"
+        guilds = len(self.bot.guilds)
+        avatar = self.bot.user.avatar_url_as(format=None, static_format='png', size=1024)
+        embed = discord.Embed(color=discord.Colour.from_rgb(245, 151, 47), description=desc)
+        embed.set_author(name=self.bot.user.name, icon_url=avatar)
+        embed.set_thumbnail(url=avatar)
+        embed.add_field(name="Tjenere", value=str(guilds))
+        embed.add_field(name="Ord i listen", value=str(len(BotSetup.bad_words)))
+        try:
+            embed.add_field(name="Rate på denne serveren", value=BotSetup.settings["rate"][str(ctx.guild.id)])
+        except KeyError:
+            embed.add_field(name="Rate på denne serveren", value="50")
+
+        await ctx.send(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(Sprakradet(bot))
+    bot.add_cog(Preposisjon(bot))
+    bot.add_cog(Info(bot))
